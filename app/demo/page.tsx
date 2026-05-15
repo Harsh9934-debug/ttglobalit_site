35afab554c4c9a8e4360058b671daa73497de294
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-05-15 07:50:48 +0530

    Updated the components

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,5 +1,11 @@
 import { DemoOne } from '@/components/demo'
+import { FaqsSection } from '@/components/ui/faqs-1'
 
 export default function DemoPage() {
-  return <DemoOne />
+  return (
+    <>
+      <DemoOne />
+      <FaqsSection />
+    </>
+  )
 }
